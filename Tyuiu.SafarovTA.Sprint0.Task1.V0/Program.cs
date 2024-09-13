71e65a69e00504286a88195095a221bdb83c82fb
SafarovTA <timursaf727272@gmail.com>
2024-09-13 17:13:12 +0300

    Added Task1

---
+++ Tyuiu.SafarovTA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.SafarovTA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Timur";
+            int age = 199;
+            Console.WriteLine("Hello, my name is "+name+", I'm "+age+" years old!");
+            Console.ReadKey();
+        }
+    }
+}
