353172f552585c85246f3dacdf38c20ba45ed085
SafarovTA <timursaf727272@gmail.com>
2024-09-14 10:40:06 +0300

    Added Task2

---
+++ Tyuiu.SafarovTA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SafarovTA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SafarovTA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Тимур";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Тимур", res);
+        }
+    }
+}
