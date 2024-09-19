4766d8bec49e34f41ac7ed549113e0e0134f80e1
SafarovTA <timursaf727272@gmail.com>
2024-09-19 11:52:21 +0300

    I have added the sixth task

---
+++ Tyuiu.SafarovTA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SafarovTA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.SafarovTA.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение = " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
