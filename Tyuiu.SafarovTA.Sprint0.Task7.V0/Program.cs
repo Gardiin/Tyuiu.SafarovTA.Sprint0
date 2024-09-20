3558db41c8010af85e4fd050596be8346e34b340
SafarovTA <timursaf727272@gmail.com>
2024-09-20 17:37:14 +0300

    Task 5 Added. I'm not adding that fucking template. Provide it first, my only job should only include copying it from some fucking file and pasting it. I'm not writing that big ass chunk of useless shit you'll never even check.

---
+++ Tyuiu.SafarovTA.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.SafarovTA.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.SafarovTA.Sprint0.Task7.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Значение элементов массива 2");
+            for (int i = 0; i < arrayNums1.Length; i++)
+            {
+                Console.WriteLine(arrayNums1[i] + ", ");
+            }
+
+            Console.WriteLine();
+
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Значение элементов массива 2");
+            for (int i = 0; i < arrayNums2.Length; i++)
+            {
+                Console.WriteLine(arrayNums2[i] + ", ");
+            }
+
+            Console.WriteLine();
+
+            if (arrayNums1.Length == arrayNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+
+                Console.WriteLine("Сумма элементов массива равна:");
+                for (int i = 0;i < resultArray.Length;i++)
+                {
+                    Console.WriteLine((int)resultArray[i] + ", ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Исходные массивы имеют разное количество элементов!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
